0adbae5a581629f99d6e836e4908d2d628441581
stern15 <ssemasuka@gmail.com>
2018-07-11 17:53:37 +0300

    cost of title added

---
+++ tile_cost.py
@@ -0,0 +1,14 @@
+"""
+Calculate the total cost of tile it would take to cover a floor plan of width and height, using a cost entered by the user.
+"""
+
+try: 
+    width = float(input("Enter the width:\n"))
+    height = float(input("Enter the height:\n"))
+    cost = float(input("Enter the cost:\n"))
+except ValueError:
+    print("Please enter a number")
+else:
+    result = lambda width,height,cost:width*height*cost
+    print("the cost is {0:.2f}".format(result(width,height,cost)))
+    
